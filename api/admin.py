dc22ccc57f9ed7603d64f54751f2108eff3f7f6e
Sari Shehada <sarishehada@gmail.com>
2024-03-13 03:22:46 +0300

    Registered the new models in admin dashboard

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Admin, Disease, DiseaseExternalLink, DiseaseMedicine, Doctor, Medicine, Patient
+
+admin.site.register(Doctor)
+admin.site.register(Admin)
+admin.site.register(Patient)
+admin.site.register(Medicine)
+admin.site.register(Disease)
+admin.site.register(DiseaseMedicine)
+admin.site.register(DiseaseExternalLink)
