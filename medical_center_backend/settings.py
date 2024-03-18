b02c292fcd6d64a056542d67e67adf0282b6c8f3
Sari Shehada <sarishehada@gmail.com>
2024-03-18 06:46:53 +0300

    Added MySQL Connection Settings

--- medical_center_backend/settings.py
+++ medical_center_backend/settings.py
@@ -81,13 +81,35 @@ WSGI_APPLICATION = 'medical_center_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# Local
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'medical_center_db',
+#         'USER': 'root',
+#         'PASSWORD': 'sari12345',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+# Hosting
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'medical_center_db',
+        'USER': 'medicalcenter24',
+        'PASSWORD': '7D9267BVac9t',
+        'HOST': 'medicalcenter24.mysql.eu.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
