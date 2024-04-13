e6fed5cd82cec791b06e1223cd9650e813f61a2f
Sari Shehada <sarishehada@gmail.com>
2024-04-13 16:36:19 +0300

    Added ended conversation validation

--- api/views/medical_case_views.py
+++ api/views/medical_case_views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
-from api.models import MedicalCaseMessage
+from api.models import MedicalCase, MedicalCaseMessage
 from api.serializers import AddMedicalCaseMessageSerializer, MedicalCaseMessageDisplaySerializer
 
 
@@ -26,6 +26,17 @@ def sendMessageToMedicalCaseChat(request, caseId):
         print('not mutable')
     requestData['caseId'] = caseId
     addMessageSerializer = AddMedicalCaseMessageSerializer(data=requestData)
+    medical_case = MedicalCase.objects.filter(id=caseId).first()
+    if (not medical_case):
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if (medical_case.status == 'ended'):
+        Response(
+            MedicalCaseMessageDisplaySerializer(
+                fetchMedicalCaseMessages(caseId=caseId),
+                many=True
+            ).data,
+            status=status.HTTP_400_BAD_REQUEST
+        )
     if addMessageSerializer.is_valid():
         addMessageSerializer.save()
         messages = fetchMedicalCaseMessages(caseId=caseId)
